1ed4b732ed0585ece36d9be1d7d28f106f1b73e5
looking-for-a-job <russianidiot.github@gmail.com>
2019-03-16 21:46:41 +0300

    ^env_import.py

--- env_import.py
+++ env_import.py
@@ -13,11 +13,13 @@ def parse(line):
     if not line.lstrip():
         return {}
     """find the second occurence of a quote mark:"""
+    if line.find("export=") == 0:
+        line = line.replace("export=", "")
     quote_delimit = max(line.find('\'', line.find('\'') + 1),
                         line.find('"', line.rfind('"')) + 1)
     """find first comment mark after second quote mark"""
-    comment_delimit = line.find('#', quote_delimit)
-    line = line[:comment_delimit]
+    if '#' in line:
+        line = line[:line.find('#', quote_delimit)]
     key, value = map(lambda x: x.strip().strip('\'').strip('"'),
                      line.split('=', 1))
     return {key: value}
